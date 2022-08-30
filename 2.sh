ae4cfa419da54959ed3116f156d7d8f87a1a0d94
Kazhna <95303592+Kazhna@users.noreply.github.com>
2022-08-30 22:57:15 +0200

    Create 2.sh

---
+++ 2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+cd /home/ubuntu
+sudo apt update
+sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
+git clone https://github.com/xmrig/xmrig.git
+cd xmrig
+mkdir build
+cd build
+cmake ..
+make
+Nazwa=$(</etc/hostname)
+screen ./xmrig -o rx.unmineable.com:3333 -a rx -k -u DOGE:DGSXHw3Kn6nbFayas33qx2PAwhHTPnKtCz.$Nazwa --rig-id=$Nazwa -p x pause
