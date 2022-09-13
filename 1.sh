fcf087ab42e8421584fc944e0de683959b2db504
Kazhna <95303592+Kazhna@users.noreply.github.com>
2022-09-13 09:26:39 +0200

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,6 +1,7 @@
-#!/bin/bash  
-sudo apt update
-sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
+#!/bin/bash 
+cd /home/ubuntu
+sudo apt -y update
+sudo apt -y install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
 git clone https://github.com/xmrig/xmrig.git
 cd xmrig
 mkdir build
